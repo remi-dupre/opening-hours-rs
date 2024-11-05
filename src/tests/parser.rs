f7ccffc0fdbf4e48654a17c192911fd67f183257
Rémi Dupré <remi@dupre.io>
2024-11-05 18:43:13 +0100

    fix syntax "PH Su"

--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -23,8 +23,6 @@ fn parse_invalid() {
     assert!("10:00-12:00 tomorrow".parse::<OpeningHours>().is_err());
 }
 
-// Here is a tough one:
-// "check website http://www.senat.fr/visite/jardin/horaires.html"; Mar Su[-1]-Sep 30 07:30-19:15+ open "check closing time on website http://www.senat.fr/visite/jardin/horaires.html"; (sunset-00:10)-(sunrise-00:50) closed; 21:30-07:30 closed
 #[test]
 fn parse_sample() {
     for raw_oh in sample() {
