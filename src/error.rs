9e9651883a7244e6593e563e782cc615034486fa
Rémi Dupré <remi@dupre.io>
2024-11-18 14:00:05 +0000

    remove thiserror dependency

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,14 @@
+use std::fmt::Display;
+
 pub use opening_hours_syntax::error::Error as ParserError;
 
-#[derive(thiserror::Error, Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
-#[error("Unknown ISO code `{0}`")]
+#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct UnknownCountryCode(pub String);
+
+impl Display for UnknownCountryCode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Unknown ISO code `{}`", self.0)
+    }
+}
+
+impl std::error::Error for UnknownCountryCode {}
