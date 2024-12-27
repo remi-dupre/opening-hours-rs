9561e47d72a559f4ee266ef69dd9af61376d348e
Rémi Dupré <remi@dupre.io>
2024-12-27 16:47:34 +0100

    run python tests in clean context

--- python/src/tests/mod.rs
+++ python/src/tests/mod.rs
@@ -4,15 +4,51 @@ mod doctests;
 use crate::opening_hours;
 use std::ffi::CString;
 use std::fmt::Write;
-use std::sync::{Arc, LazyLock, Mutex, Once};
+use std::hash::{Hash, Hasher};
+use std::sync::{Arc, Mutex, Once};
 
 use pyo3::prelude::*;
 
-static INIT_PYTHON_GIL: Once = Once::new();
+pub(crate) fn test_fn_name(source: &str) -> String {
+    let mut hash = std::hash::DefaultHasher::new();
+    source.hash(&mut hash);
+    format!("test_python_{:x}", hash.finish())
+}
+
+pub(crate) fn wrap_with_function(source: &str) -> String {
+    let fn_name = test_fn_name(source);
+    let common_prefix = source
+        .lines()
+        .filter(|l| !l.trim().is_empty())
+        .reduce(|mut prefix, line| {
+            while !line.starts_with(prefix) {
+                prefix = &prefix[..prefix.len() - 1];
+            }
+
+            prefix
+        })
+        .unwrap_or("");
+
+    let mut output = source
+        .lines()
+        .fold(format!("def {fn_name}():"), |mut acc, line| {
+            writeln!(
+                &mut acc,
+                "    {}",
+                line.strip_prefix(common_prefix).unwrap_or(""),
+            )
+            .unwrap();
+            acc
+        });
+
+    writeln!(&mut output, "{fn_name}()").unwrap();
+    output
+}
 
 pub(crate) fn run_python(source: &str) {
-    static GIL_LOCK: LazyLock<Mutex<()>> = LazyLock::new(Mutex::default);
+    static INIT_PYTHON_GIL: Once = Once::new();
 
+    // Capture stdout into a buffer
     #[pyclass]
     struct CaptureStdout(Arc<Mutex<String>>);
 
@@ -28,35 +64,16 @@ pub(crate) fn run_python(source: &str) {
         }
     }
 
-    let common_prefix = source
-        .lines()
-        .filter(|l| !l.trim().is_empty())
-        .reduce(|mut prefix, line| {
-            while !line.starts_with(prefix) {
-                prefix = &prefix[..prefix.len() - 1];
-            }
+    // Remove indentation and wrap test into a function
+    let source_wrapped = wrap_with_function(source);
 
-            prefix
-        })
-        .unwrap_or("");
-
-    let without_indent = source.lines().fold(String::new(), |mut acc, line| {
-        writeln!(
-            &mut acc,
-            "{}",
-            line.strip_prefix(common_prefix).unwrap_or("")
-        )
-        .unwrap();
-        acc
-    });
-
+    // Start GIL once per session
     INIT_PYTHON_GIL.call_once(|| {
         pyo3::append_to_inittab!(opening_hours);
         pyo3::prepare_freethreaded_python();
     });
 
-    let _guard = GIL_LOCK.lock().expect("could not get exclusive GIL lock");
-
+    // Run test
     Python::with_gil(|py| {
         let sys = py.import("sys").expect("could not import sys");
         let buffer: Arc<Mutex<String>> = Arc::default();
@@ -67,7 +84,7 @@ pub(crate) fn run_python(source: &str) {
         )
         .expect("could not intercept stdout");
 
-        if let Err(err) = py.run(CString::new(without_indent).unwrap().as_c_str(), None, None) {
+        if let Err(err) = py.run(CString::new(source_wrapped).unwrap().as_c_str(), None, None) {
             let traceback = err
                 .traceback(py)
                 .and_then(|tb| tb.format().ok())
