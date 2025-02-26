10e9bcc33b62f3477b43be2237934aab0689b863
Rémi Dupré <remi@dupre.io>
2025-02-26 21:10:31 +0100

    update benchmarking suite

--- opening-hours/benches/benchmarks.rs
+++ opening-hours/benches/benchmarks.rs
@@ -4,23 +4,28 @@ use opening_hours::{Context, OpeningHours};
 use chrono::NaiveDateTime;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-const SCH_24_7: &str = "24/7";
-const SCH_ADDITION: &str = "10:00-12:00 open, 14:00-16:00 unknown, 16:00-23:00 closed";
-const SCH_HOLIDAY: &str = "PH";
-const SCH_JAN_DEC: &str = "Jan-Dec";
+const SAMPLES: &[[&str; 2]] = &[
+    ["24_7", "24/7"],
+    ["holidays", "Mo-Fr 10:00-18:00 ; PH off"],
+    [
+        "rule_normal",
+        "Mo-Fr 10:00-12:00,14:00-18:00 ; Sa-Su 10:00-14:00 unknown ; Dec31 off",
+    ],
+    [
+        "rule_addition",
+        "Mo-Fr 10:00-18:00 , Sa-Su 10:00-14:00 unknown, 12:00-14:00 closed",
+    ],
+    [
+        "rule_fallback",
+        "Mo-Fr 10:00-12:00,14:00-18:00 ; Sa 10:00-13:00 || 10:00-12:00 unknown",
+    ],
+    [
+        "huge",
+        "Aug:Sa;week50unknown;Nov;2492week9:;:Mo;Fr;1912week48;:Mo;7591-1918week1:;:Mo;week8Sa;1918week3:;:Mo;7191-1911Mo;MayWe;Fr;week2;Feb;Oct;3683;Fr;1915week48;:Mo;5182-1919week1:;:Mo;week8Sa;1918week4:;:Mo;7191-1911;Mo;MayWe;Fr;week2;Feb;Oct;3836week3:;:Th;Su;3818closed; Fr;1917week17",
+    ],
+];
+
 const PARIS_COORDS: Coordinates = Coordinates::new(48.8535, 2.34839).unwrap();
-
-fn bench_parse(c: &mut Criterion) {
-    let mut group = c.benchmark_group("parse");
-
-    group.bench_function("24_7", |b| {
-        b.iter(|| OpeningHours::parse(black_box(SCH_24_7)).unwrap())
-    });
-
-    group.bench_function("addition", |b| {
-        b.iter(|| OpeningHours::parse(black_box(SCH_ADDITION)).unwrap())
-    });
-}
 
 fn bench_context(c: &mut Criterion) {
     let mut group = c.benchmark_group("context");
@@ -30,28 +35,38 @@ fn bench_context(c: &mut Criterion) {
     });
 }
 
-fn bench_eval(c: &mut Criterion) {
+fn bench_sample(c: &mut Criterion) {
     let fr_context = Context::default().with_holidays(Country::FR.holidays());
     let date_time = NaiveDateTime::parse_from_str("2021-02-01 12:03", "%Y-%m-%d %H:%M").unwrap();
 
-    let expressions = [
-        ("24_7", OpeningHours::parse(SCH_24_7).unwrap()),
-        ("addition", OpeningHours::parse(SCH_ADDITION).unwrap()),
-        ("holidays", OpeningHours::parse(SCH_HOLIDAY).unwrap()),
-        (
-            "jan-dec",
-            OpeningHours::parse(SCH_JAN_DEC)
-                .unwrap()
-                .with_context(fr_context),
-        ),
-    ];
+    let sample_oh: Vec<_> = SAMPLES
+        .iter()
+        .map(|[slug, expr]| {
+            (
+                *slug,
+                OpeningHours::parse(expr)
+                    .unwrap()
+                    .with_context(fr_context.clone()),
+            )
+        })
+        .collect();
+
+    {
+        let mut group = c.benchmark_group("parse");
+
+        for [slug, expr] in SAMPLES {
+            group.bench_function(*slug, |b| {
+                b.iter(|| OpeningHours::parse(black_box(expr)).unwrap())
+            });
+        }
+    }
 
     {
         let mut group = c.benchmark_group("is_open");
 
-        for (slug, expr) in &expressions {
+        for (slug, oh) in &sample_oh {
             group.bench_function(*slug, |b| {
-                b.iter(|| black_box(&expr).is_open(black_box(date_time)))
+                b.iter(|| black_box(&oh).is_open(black_box(date_time)))
             });
         }
     }
@@ -59,9 +74,9 @@ fn bench_eval(c: &mut Criterion) {
     {
         let mut group = c.benchmark_group("next_change");
 
-        for (slug, expr) in &expressions {
+        for (slug, oh) in &sample_oh {
             group.bench_function(*slug, |b| {
-                b.iter(|| black_box(black_box(&expr).next_change(black_box(date_time))))
+                b.iter(|| black_box(black_box(&oh).next_change(black_box(date_time))))
             });
         }
     }
@@ -69,13 +84,11 @@ fn bench_eval(c: &mut Criterion) {
     {
         let mut group = c.benchmark_group("normalize");
 
-        for (slug, expr) in &expressions {
-            group.bench_function(*slug, |b| {
-                b.iter(|| black_box(black_box(&expr).normalize()))
-            });
+        for (slug, oh) in &sample_oh {
+            group.bench_function(*slug, |b| b.iter(|| black_box(black_box(&oh).normalize())));
         }
     }
 }
 
-criterion_group!(benches, bench_parse, bench_context, bench_eval);
+criterion_group!(benches, bench_context, bench_sample);
 criterion_main!(benches);
